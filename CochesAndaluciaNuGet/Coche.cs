b787100179291ee8b31fe39513b2a841eda64631
serraguti <46522403+serraguti@users.noreply.github.com>
2022-01-18 16:31:34 +0100

    Add project files.

---
+++ CochesAndaluciaNuGet/Coche.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CochesAndaluciaNuGet
+{
+    public class Coche
+    {
+        public int IdCoche { get; set; }
+        public String Marca { get; set; }
+        public String Modelo { get; set; }
+        public String Imagen {get;set;}
+    }
+}
